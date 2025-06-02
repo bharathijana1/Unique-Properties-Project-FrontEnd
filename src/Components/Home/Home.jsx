0cd4d48396af7d4df2f951d7a0d7540e84c2f5ff
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-06-02 16:56:29 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -74,7 +74,7 @@ export default function Home() {
           />
           </h1>
 
-          <p className="text-xl text-center text-[#EADEC9] pt-2">
+          <p className="text-xl text-center text-[#EADEC9] pt-4">
             I deliver the very best in all facets of real estate
           </p>
           <p className="text-xl text-[#EADEC9] mb-8 text-center">
